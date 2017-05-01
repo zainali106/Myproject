4550b88e9a088475539a6b27199700603f334caa
Muhammad Zain Ali <Muhammad Zain Ali>
2017-05-01 17:31:55 +0500

    First commit

---
+++ listofprocess/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace listofprocess
+{
+    class Program
+    {
+      
+        static int count = 0;
+        static void Main()
+        {
+
+            var stopwatch = Stopwatch.StartNew();
+            Thread.Sleep(0);
+            stopwatch.Stop();
+            Console.WriteLine("waiting ...");
+            //Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            stopwatch = Stopwatch.StartNew();
+            Thread.Sleep(5000);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            stopwatch = Stopwatch.StartNew();
+            System.Threading.Thread.Sleep(1000);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+            //
+            // Bonus: shows SpinWait method.
+            //
+            stopwatch = Stopwatch.StartNew();
+            Thread.SpinWait(100000 * 10000);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+       
+    }
+}
